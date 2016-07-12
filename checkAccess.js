98f7d0fd286e6c2fa5b4c445ca0e8a55a7c23030
Leo Farias <leokcta@gmail.com>
2016-07-11 22:24:12 -0400

    Implemented result detail and improved network check

--- checkAccess.js
+++ checkAccess.js
@@ -1,49 +1,77 @@
 var requestPermission;
+var editPermission;
 
 function check(e){
     requestPermission = e.requestPermission;
-    if (e.camera) { camera(); } 
+    // TO DO - created check method for all the permissions
 }
 
 function network(e){
     var _hasNetworkConnection = Titanium.Network.online;
-    return _hasNetworkConnection;
+    var _networkType = Ti.Network.networkType;
+    var _detail;
+
+    if (_networkType === Ti.Network.NETWORK_MOBILE) {
+        _detail = "Device is communicating over a mobile network";
+        console.log('ti.checkAccess => ' + _detail);
+    } else if( _networkType === Ti.Network.NETWORK_WIFI ){
+        _detail = "Device is communicating over a mobile network";
+        console.log('ti.checkAccess => ' + _detail);
+    } else if( _networkType === Ti.Network.NETWORK_NONE ){
+        _detail = "No network is available";
+        console.log('ti.checkAccess => ' + _detail);
+    }
+
+    return result(_hasNetworkConnection, _detail);
 }
 
 function camera(e){
-    //check camera permission
-    var _hasCameraPermissions = Ti.Media.hasCameraPermissions();
+
+    var _detail;
 
     if (OS_IOS) {
         var cameraAuthorizationStatus = Ti.Media.cameraAuthorizationStatus;
 
         if (cameraAuthorizationStatus === Ti.Media.CAMERA_AUTHORIZATION_RESTRICTED) {
-			console.warn('ti.checkAccess =>  Because permission are restricted by some policy which you as user cannot change, we don\'t request as that might also cause issues.');
+			_detail = "Because permission are restricted by some policy which you as user cannot change, we don\'t request as that might also cause issues.";
+            console.warn('ti.checkAccess => ' + _detail);
 		} else if (cameraAuthorizationStatus === Ti.Media.CAMERA_AUTHORIZATION_DENIED) {
-            console.warn('ti.checkAccess =>  Permission has been denied before.')
+            _detail = "Permission has been denied before.";
+            console.warn('ti.checkAccess =>  ' + _detail);
         }
     }
 
     //if we dont have to request permission
-    if(!requestPermission){
-        return _hasCameraPermissions;
+    if(requestPermission){
+        //Before you request permission on android make sure you have this set on tiapp.xml
+        // <android xmlns:android="http://schemas.android.com/apk/res/android">
+        //     <manifest>
+        //         <uses-permission android:name="android.permission.CAMERA" />
+        //     </manifest>
+        // </android>
+
+        Ti.Media.requestCameraPermissions(function(e){
+            if(e.success){
+                _detail = "You granted permission";
+                console.log('ti.checkAccess => ' + _detail);
+            }  else {
+                _detail = "You denied permission";
+                console.warn('ti.checkAccess => ' + _detail);
+            }
+        });
     }
 
-    //Before you request permission on android make sure you have this set on tiapp.xml
-    // <android xmlns:android="http://schemas.android.com/apk/res/android">
-    //     <manifest>
-    //         <uses-permission android:name="android.permission.CAMERA" />
-    //     </manifest>
-    // </android>
+    var _hasCameraPermissions = Ti.Media.hasCameraPermissions();
+    return result(_hasCameraPermissions, _detail);
+}
 
-    Ti.Media.requestCameraPermissions(function(e){
-        if(e.success){
-            console.log('ti.checkAccess => You granted permission');
-        }  else {
-            console.warn('ti.checkAccess => You denied permission');
-        }
-        return Ti.Media.hasCameraPermissions();
-    });
+function result(value, detail){
+    if (editPermission) goToSettings();
+
+    return {
+        "permission": value,
+        "detail": detail
+    }
 }
 
 exports.check = check;
