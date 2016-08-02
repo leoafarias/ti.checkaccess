cbb42efb94d06e82cdd159d3e7d08184da1de128
Leo Farias <leokcta@gmail.com>
2016-08-01 22:06:47 -0400

    Created widget, and removed sample app and duplicate lib code from root

---
+++ ti.checkpermission/controllers/widget.js
@@ -0,0 +1,42 @@
+var check = require(WPATH('checkAccess')).check;
+
+function checkAll(e){
+	var _types = ['network','camera', 'calendar', 'contacts', 'geolocation'];
+	var _results = check.permissions(_types);
+
+	_.each(_results, function(type){
+		handleReturn(type.permission, type);
+	});
+}
+
+function doCheck(e){
+	var result = check[e.source.id]();
+	handleReturn(e.source.id, result);
+}
+
+function handleReturn(elem, result){
+	
+	
+	if(result && result.access){
+		$[elem].applyProperties({
+			backgroundColor: '#006400'
+		});
+	} else {
+		$[elem].applyProperties({
+			backgroundColor: '#8b0000'
+		});
+	}
+}
+
+/**
+ * Event Handler
+ */
+
+function onRequestPermissionSwitch(e){
+	check.requestPermission = e.value;
+	console.log(e.value);
+}
+
+$.permissionView.open();
+
+checkAll();
